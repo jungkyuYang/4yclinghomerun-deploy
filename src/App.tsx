581753126ef526c5689611b50ff551b5cf69ae0e
bbjbc <jobc4550@gmail.com>
2024-08-31 00:46:43 +0900

    chore: init project setting

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex min-h-screen justify-center">
+      <h1 className="m-10 flex items-center justify-center rounded-2xl p-10 text-3xl font-bold">
+        Hi, there! We are 4yclingHomerun
+      </h1>
+    </div>
+  );
+};
+
+export default App;
