73f07383625f9a0f61533f5424fc39fdf1c6710a
bbjbc <jobc4550@gmail.com>
2024-08-31 00:49:37 +0900

    feat: add pretendard font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ const App = () => {
   return (
     <div className="flex min-h-screen justify-center">
       <h1 className="m-10 flex items-center justify-center rounded-2xl p-10 text-3xl font-bold">
-        Hi, there! We are 4yclingHomerun
+        Hi, there! We are 4yclingHomerun / 안녕하세요! 4yclingHomerun입니다. /
+        글씨체 Pretendard
       </h1>
     </div>
   );
