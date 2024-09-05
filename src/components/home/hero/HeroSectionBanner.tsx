7dd6a2daf561faf33f493e47d5627e0ba57b071f
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-09-05 09:34:21 +0900

    feat: 히어로 섹션 스크롤 애니메이션 구현

---
+++ src/components/home/hero/HeroSectionBanner.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react';
+
+import { motion, useAnimation, useScroll } from 'framer-motion';
+import { SlArrowDown } from 'react-icons/sl';
+
+import heroImg from '../../../assets/images/home/hero_banner.jpg';
+
+const HeroSectionBanner = () => {
+  const bannerControls = useAnimation();
+  const arrowControls = useAnimation();
+  const { scrollYProgress } = useScroll();
+
+  useEffect(() => {
+    arrowControls.start({
+      y: [0, 20, 0],
+      transition: {
+        duration: 1.5,
+        ease: 'easeInOut',
+        repeat: Infinity,
+      },
+    });
+
+    const handleScroll = () => {
+      const progress = scrollYProgress.get();
+
+      if (progress > 0 && progress < 0.1) {
+        bannerControls.start({ opacity: 1 });
+        arrowControls.start({ opacity: 0.5 });
+      } else if (progress > 0.15) {
+        arrowControls.start({ opacity: 0 });
+        bannerControls.start({ opacity: 0 });
+      }
+    };
+
+    scrollYProgress.on('change', handleScroll);
+  }, [scrollYProgress, bannerControls]);
+
+  return (
+    <section className="relative h-screen">
+      <motion.img
+        src={heroImg}
+        className="h-full w-full object-cover"
+        initial={{ opacity: 1 }}
+        animate={bannerControls}
+      />
+      <motion.div
+        className="absolute bottom-16 left-[48%]"
+        initial={{ opacity: 0.5 }}
+        animate={arrowControls}
+      >
+        <SlArrowDown size="70px" color="#ECEEF2" />
+      </motion.div>
+    </section>
+  );
+};
+export default HeroSectionBanner;
---
+++ src/components/home/hero/HeroSectionBranding.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+
+import { motion, useAnimation, useInView } from 'framer-motion';
+
+import winningKt from '../../../assets/images/home/winning_kt.webp';
+
+const HeroSectionBranding = () => {
+  const controls = useAnimation();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  useEffect(() => {
+    if (isInView === false) {
+      controls.set({ opacity: 0 });
+    } else {
+      controls.start({ opacity: 1 });
+    }
+  }, [isInView]);
+
+  return (
+    <section className="relative h-screen">
+      <motion.img
+        ref={ref}
+        src={winningKt}
+        initial={{ opacity: 0 }}
+        animate={controls}
+        transition={{ duration: 1, delay: 1 }}
+      />
+    </section>
+  );
+};
+export default HeroSectionBranding;
