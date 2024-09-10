3cc172a83ca1f6f78aed1125493db509163ba7ba
hdayeon <gkekdus97@naver.com>
2024-09-10 21:15:47 +0900

    style: 이중 타이틀, 버튼 제거 및 텍스트 사이즈 미세조정

--- src/components/home/contents/shop/Shop.tsx
+++ src/components/home/contents/shop/Shop.tsx
@@ -2,18 +2,11 @@ import { randomFiveItems } from '@/mocks/home/ShopItem';
 
 const Shop = () => {
   const shopItems = randomFiveItems;
-  // 가장 큰 사이즈로 나올 메인아이템 제외한 나머지
+  // 가장 큰 사이즈로 나올 메인아이템 제외
   const restItems = shopItems.slice(1);
   return (
     <>
-      <div className="mb-6 flex justify-between">
-        <h2 className="text-4xl">Shop</h2>
-        {/* 공통스타일 추후 합쳐서 빼기 - transition 효과 */}
-        <button className="w-32 border py-3 transition-colors duration-300 hover:bg-white hover:text-black">
-          더보기
-        </button>
-      </div>
-      <div className="m-auto max-w-[1500px]">
+      <div className="m-auto mt-4 max-w-[1500px]">
         {/* 추후 반응형 논의 - xl */}
         <ul className="flex justify-between gap-4 xl:gap-8">
           {/* 메인 아이템 */}
@@ -27,8 +20,8 @@ const Shop = () => {
               alt={`shop item ${shopItems[0].id}`}
             />
             <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-70 opacity-0 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100">
-              <h3 className="text-2xl">{shopItems[0].title}</h3>
-              <h4 className="text-md">{shopItems[0].price}</h4>
+              <h3 className="text-xl">{shopItems[0].title}</h3>
+              <h4 className="text-sm">{shopItems[0].price}</h4>
             </div>
           </li>
 
@@ -46,9 +39,11 @@ const Shop = () => {
                     src={item.src}
                     alt={`shop item ${item.id}`}
                   />
-                  <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100">
-                    <h3 className="text-2xl">{item.title}</h3>
-                    <h4 className="text-md">{item.price}</h4>
+                  <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 p-2 opacity-0 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100 xl:p-5">
+                    <h3 className="xl:text-md text-sm leading-none">
+                      {item.title}
+                    </h3>
+                    <h4 className="text-sm">{item.price}</h4>
                   </div>
                 </li>
               ))}
