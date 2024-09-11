3fe3deb168c184b1c67a1b48d0fa613d7133ae58
bbjbc <jobc4550@gmail.com>
2024-09-11 10:39:57 +0900

    feat: 팀 데이터 및 경기 결과 목데이터 삽입

---
+++ src/mocks/game/TeamInfo.ts
@@ -0,0 +1,23 @@
+import doosanImg from '@/assets/team/doosan.png';
+import lgImg from '@/assets/team/lg.png';
+import lotteImg from '@/assets/team/lotte.png';
+import ncImg from '@/assets/team/nc.png';
+import samsungImg from '@/assets/team/samsung.png';
+import ssgImg from '@/assets/team/ssg.png';
+import kiaImg from '@/assets/team/kia.png';
+import hanhwaImg from '@/assets/team/hanhwa.png';
+import kiwoomImg from '@/assets/team/kiwoom.png';
+
+const TeamInfo = [
+  { name: '두산 베어스', img: doosanImg, code: 'DOOSAN', stadium: '잠실' },
+  { name: 'LG 트윈스', img: lgImg, code: 'LG', stadium: '잠실' },
+  { name: '롯데 자이언츠', img: lotteImg, code: 'LOTTE', stadium: '사직' },
+  { name: 'NC 다이노스', img: ncImg, code: 'NC', stadium: '창원' },
+  { name: '삼성 라이온즈', img: samsungImg, code: 'SAMSUNG', stadium: '대구' },
+  { name: 'SSG 랜더스', img: ssgImg, code: 'SSG', stadium: '문학' },
+  { name: 'KIA 타이거즈', img: kiaImg, code: 'KIA', stadium: '광주' },
+  { name: '한화 이글스', img: hanhwaImg, code: 'HANHWA', stadium: '대전' },
+  { name: '키움 히어로즈', img: kiwoomImg, code: 'KIWOOM', stadium: '고척' },
+];
+
+export { TeamInfo };
