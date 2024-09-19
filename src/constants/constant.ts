773c209cf9ee08832749a7c9bde48c3d66006d29
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2024-09-19 23:22:05 +0900

    fix: logourl 경로 수정

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -26,7 +26,7 @@ export const KT_RECRUIT_URL = 'https://recruit.kt.com/';
 export const KT_PARTNER_URL = 'https://partner.kt.com/';
 export const KT_WIZ_STORE_URL = 'https://www.ktwizstore.co.kr/';
 
-export const LOGO_URL = '/src/assets/images/logo/KTwiz_logo.svg';
+export const LOGO_URL = '/src/assets/logo/KTwiz_logo.svg';
 
 export const ROUTER_PATH = {
   HOME: '/',
