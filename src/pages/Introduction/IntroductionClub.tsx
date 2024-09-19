2665471941f1b08a9763731caf4e7dbc80c49988
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-09-19 11:38:14 +0900

    refactor: map 함수 key prop 경고 개선

--- src/pages/Introduction/IntroductionClub.tsx
+++ src/pages/Introduction/IntroductionClub.tsx
@@ -6,7 +6,7 @@ const IntroductionClub = () => {
   return (
     <div className="flex flex-col gap-36">
       {IntroductionClubData.map((data, index) => (
-        <IntroductionClubItem data={data} index={index} />
+        <IntroductionClubItem key={data.id} data={data} index={index} />
       ))}
     </div>
   );
