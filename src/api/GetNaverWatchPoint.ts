367d1bfab237134527aab0e7427764c1e6a28d45
bbjbc <jobc4550@gmail.com>
2024-09-21 02:04:24 +0900

    refactor: useAxios훅의 가공 데이터 사용으로 인한 리팩토링

--- src/api/GetNaverWatchPoint.ts
+++ src/api/GetNaverWatchPoint.ts
@@ -23,78 +23,51 @@ interface GetNaverWatchPointResponse {
   };
 }
 
-const currentSeasonStatsInitialState: WatchPointPitcherStatType = {
-  bb: 0,
-  er: 0,
-  era: '',
-  gday: '',
-  hit: 0,
-  hr: 0,
-  inn2: '',
-  kk: 0,
-  l: 0,
-  s: 0,
-  teamName: '',
-  teamCode: '',
-  w: 0,
-  whip: '',
-};
-
-const currentSeasonStatsOnOpponentsInitialState: PitcherStatOnOpponentType = {
-  l: 0,
-  s: 0,
-  w: 0,
-  inn: '',
-  kk: 0,
-  era: '',
-  gameCount: 0,
-  er: 0,
-};
-
-const pitcherInfoInitialState: PitchInfoType = {
-  backnum: '',
-  birth: '',
-  height: '',
-  hitType: '',
-  name: '',
-  pCode: '',
-  weight: '',
-};
-
 const GetNaverWatchPoint = (gmkey: string) => {
-  const { data, isLoading, isError, error } =
-    useAxios<GetNaverWatchPointResponse>({
-      method: 'GET',
-      url: `/game/navergameinfo?gmkey=${gmkey}`,
-      initialData: {
-        result: {
-          previewData: {
-            homeStarter: {
-              currentPitKindStats: [],
-              currentSeasonStats: currentSeasonStatsInitialState,
-              currentSeasonStatsOnOpponents:
-                currentSeasonStatsOnOpponentsInitialState,
-              playerInfo: pitcherInfoInitialState,
-            },
-            awayStarter: {
-              currentPitKindStats: [],
-              currentSeasonStats: currentSeasonStatsInitialState,
-              currentSeasonStatsOnOpponents:
-                currentSeasonStatsOnOpponentsInitialState,
-              playerInfo: pitcherInfoInitialState,
-            },
+  const processData = (data: GetNaverWatchPointResponse) => {
+    const { homeStarter, awayStarter } = data.result.previewData;
+    return {
+      home: {
+        currentPitKindStats: homeStarter.currentPitKindStats,
+        currentSeasonStats: homeStarter.currentSeasonStats,
+        currentSeasonStatsOnOpponents:
+          homeStarter.currentSeasonStatsOnOpponents,
+        playerInfo: homeStarter.playerInfo,
+      },
+      away: {
+        currentPitKindStats: awayStarter.currentPitKindStats,
+        currentSeasonStats: awayStarter.currentSeasonStats,
+        currentSeasonStatsOnOpponents:
+          awayStarter.currentSeasonStatsOnOpponents,
+        playerInfo: awayStarter.playerInfo,
+      },
+    };
+  };
+
+  return useAxios<GetNaverWatchPointResponse, ReturnType<typeof processData>>({
+    method: 'GET',
+    url: `/game/navergameinfo?gmkey=${gmkey}`,
+    initialData: {
+      result: {
+        previewData: {
+          homeStarter: {
+            currentPitKindStats: [],
+            currentSeasonStats: {} as WatchPointPitcherStatType,
+            currentSeasonStatsOnOpponents: {} as PitcherStatOnOpponentType,
+            playerInfo: {} as PitchInfoType,
+          },
+          awayStarter: {
+            currentPitKindStats: [],
+            currentSeasonStats: {} as WatchPointPitcherStatType,
+            currentSeasonStatsOnOpponents: {} as PitcherStatOnOpponentType,
+            playerInfo: {} as PitchInfoType,
           },
         },
       },
-      shouldFetchOnMount: !!gmkey,
-    });
-
-  return {
-    data,
-    error,
-    isLoading,
-    isError,
-  };
+    },
+    shouldFetchOnMount: !!gmkey,
+    processData,
+  });
 };
 
 export { GetNaverWatchPoint };
--- src/pages/game/WatchPointPage.tsx
+++ src/pages/game/WatchPointPage.tsx
@@ -20,6 +20,17 @@ const WatchPointPage = () => {
   const { year, month } = useScheduleStore();
 
   const { data: monthScheduleData } = GetMonthSchedule(year, month);
+  const {
+    data: watchPointData,
+    isLoading: watchPointLoading,
+    error: watchPointError,
+  } = GetWatchPoint(finalGameDate, finalGmkey);
+
+  const {
+    data: naverWatchPointData,
+    isLoading: naverWatchPointIsLoading,
+    error: naverWatchPointError,
+  } = GetNaverWatchPoint(finalGmkey);
 
   // 가장 최근 경기의 gameDate와 gmkey를 가져옴.
   useEffect(() => {
@@ -39,90 +50,74 @@ const WatchPointPage = () => {
     setFinalGmkey(gmkey || lastFinishedGame?.gmkey || '');
   }, [location.search, year, month, monthScheduleData]);
 
-  const {
-    data: watchPointData,
-    isLoading: watchPointLoading,
-    error: watchPointError,
-  } = GetWatchPoint(finalGameDate, finalGmkey);
-
-  const {
-    data: naverWatchPointData,
-    isLoading: naverWatchIsLoading,
-    isError: naverWatchIsError,
-    error,
-  } = GetNaverWatchPoint(finalGmkey);
-
   const handleNextGame = () => {
-    if (watchPointData?.data.schedule.next) {
+    if ('schedule' in watchPointData && watchPointData.schedule.next) {
       const { gameDate: nextGameDate, gmkey: nextGmkey } =
-        watchPointData.data.schedule.next;
+        watchPointData.schedule.next;
       navigate(`/game/watchpoint?${nextGameDate}&${nextGmkey}`);
     }
   };
 
   const handlePrevGame = () => {
-    if (watchPointData?.data.schedule.prev) {
+    if ('schedule' in watchPointData && watchPointData.schedule.prev) {
       const { gameDate: prevGameDate, gmkey: prevGmkey } =
-        watchPointData.data.schedule.prev;
+        watchPointData.schedule.prev;
       navigate(`/game/watchpoint?${prevGameDate}&${prevGmkey}`);
     }
   };
 
-  if (watchPointError || naverWatchIsError)
-    return <div>Error: {watchPointError || error}</div>;
-  if (watchPointLoading || naverWatchIsLoading) return <WatchPointSkeleton />;
-  if (!watchPointData?.data || !naverWatchPointData?.result) return null;
+  if (watchPointError || naverWatchPointError)
+    return <div>Error: {watchPointError || naverWatchPointError}</div>;
+  if (
+    !watchPointData ||
+    !('gameScore' in watchPointData) ||
+    !('schedule' in watchPointData) ||
+    !('homeTeam' in watchPointData) ||
+    !('visitTeam' in watchPointData) ||
+    !naverWatchPointData ||
+    !('home' in naverWatchPointData) ||
+    !('away' in naverWatchPointData)
+  )
+    return null;
 
   return (
     <div className="flex flex-col gap-10">
       <div>
         <SectionHeading title="상대 전적 비교" />
-        <WatchPointItem
-          game={watchPointData.data.gameScore}
-          homeGameRecord={watchPointData.data.homeTeamRank}
-          awayGameRecord={watchPointData.data.visitTeamRank}
-          homeVsRecord={watchPointData.data.homeTeamWinLose}
-          awayVsRecord={watchPointData.data.visitTeamWinLose}
-          onNextGame={handleNextGame}
-          onPrevGame={handlePrevGame}
-          hasNextGame={!!watchPointData.data.schedule.next}
-          hasPrevGame={!!watchPointData.data.schedule.prev}
-        />
+        {watchPointLoading || naverWatchPointIsLoading ? (
+          <WatchPointSkeleton />
+        ) : (
+          <WatchPointItem
+            game={watchPointData.gameScore}
+            homeGameRecord={watchPointData.homeTeam.rank}
+            awayGameRecord={watchPointData.visitTeam.rank}
+            homeVsRecord={watchPointData.homeTeam.vsRecord}
+            awayVsRecord={watchPointData.visitTeam.vsRecord}
+            onNextGame={handleNextGame}
+            onPrevGame={handlePrevGame}
+            hasNextGame={!!watchPointData.schedule.next}
+            hasPrevGame={!!watchPointData.schedule.prev}
+          />
+        )}
       </div>
 
       <div>
         <SectionHeading title="선발 투수 비교" />
         <PitcherStatCompareItem
           homeCurrentPitKindStats={
-            naverWatchPointData?.result.previewData.homeStarter
-              .currentPitKindStats
+            naverWatchPointData?.home?.currentPitKindStats
           }
-          homeCurrentSeasonStats={
-            naverWatchPointData?.result.previewData.homeStarter
-              .currentSeasonStats
-          }
+          homeCurrentSeasonStats={naverWatchPointData.home.currentSeasonStats}
           homeCurrentSeasonStatsOnOpponents={
-            naverWatchPointData?.result.previewData.homeStarter
-              .currentSeasonStatsOnOpponents
+            naverWatchPointData.home.currentSeasonStatsOnOpponents
           }
-          awayCurrentPitKindStats={
-            naverWatchPointData?.result.previewData.awayStarter
-              .currentPitKindStats
-          }
-          awayCurrentSeasonStats={
-            naverWatchPointData?.result.previewData.awayStarter
-              .currentSeasonStats
-          }
+          homePlayerInfo={naverWatchPointData.home.playerInfo}
+          awayCurrentPitKindStats={naverWatchPointData.away.currentPitKindStats}
+          awayCurrentSeasonStats={naverWatchPointData.away.currentSeasonStats}
           awayCurrentSeasonStatsOnOpponents={
-            naverWatchPointData?.result.previewData.awayStarter
-              .currentSeasonStatsOnOpponents
+            naverWatchPointData.away.currentSeasonStatsOnOpponents
           }
-          homePlayerInfo={
-            naverWatchPointData?.result.previewData.homeStarter.playerInfo
-          }
-          awayPlayerInfo={
-            naverWatchPointData?.result.previewData.awayStarter.playerInfo
-          }
+          awayPlayerInfo={naverWatchPointData.away.playerInfo}
         />
       </div>
     </div>
