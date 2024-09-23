da1d28a17fb596c97b86e39ccb7c1007de93ecee
bbjbc <jobc4550@gmail.com>
2024-09-24 01:07:30 +0900

    refactor: 기존 애니메이션 빼고 framer motion으로 변경

--- src/components/game/watchpoint/common/PlayerAvatar.tsx
+++ src/components/game/watchpoint/common/PlayerAvatar.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+
 import { TopPlayerInfoType, PitchInfoType } from '@/types/WatchPointType';
 
 type PlayerAvatarProps = {
@@ -10,7 +12,17 @@ const PlayerAvatar = ({ player, isLoading, imageUrl }: PlayerAvatarProps) => {
   return (
     <figure className="text-center">
       {isLoading ? (
-        <div className="animate-pulse h-32 w-32 rounded-full border-2 border-kt-gray-2 bg-gray-800"></div>
+        <motion.div
+          className="h-32 w-32 rounded-full border-2 border-kt-gray-2 bg-gray-800"
+          animate={{
+            opacity: [0.2, 1, 0.2],
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 1,
+            ease: 'easeInOut',
+          }}
+        ></motion.div>
       ) : (
         <img
           src={imageUrl}
